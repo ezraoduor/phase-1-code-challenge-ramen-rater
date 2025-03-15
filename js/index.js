cbf39d205a624b803ad9d172abd3bcdb505e8e18
Ezra Oduor <ezrachurchill20@gmail.com>
2025-03-15 23:23:27 +0300

    added event listerner

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const ramenOptions = document.getElementById("ramen-options");
-    const ramenMenu = document.getElementById("ramen-menu");
-    const ramenDetailImage = document.getElementById("ramen-detail-image");
-    const ramenName = document.getElementById("ramen-name");
-    const ramenRestaurant = document.getElementById("ramen-restaurant");
-    const ramenRating = document.getElementById("ramen-rating");
-    const ramenComment = document.getElementById("ramen-comment");
-    const ramenForm = document.getElementById("new-ramen-form");
-     
-    const ramens = [
-        { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-        { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-        { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-     ];
+document.addEventListener("DOMContentLoaded", main);
+
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "assets/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "assets/miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "assets/tonkotsu.jpg", rating: 6, comment: "very good!" } ,
+    { id: 4, name: "Kojiro ramen", restaurant: "Meko", image: "assets/kojiro.jpg", rating: 6, comment: "very yummy!" } 
+ ];
+
